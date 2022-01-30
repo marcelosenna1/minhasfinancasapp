9fa39b628f9b3f264759290a24c6a3387452d396
Marcelo Sena <marcelomarcos2@gmail.com>
2022-01-30 20:47:38 -0300

    Terminando testes de integração de Usuarios

--- backend/src/test/java/com/sena/minhasfinancas/model/repositories/UsuarioRepositoryTest.java
+++ backend/src/test/java/com/sena/minhasfinancas/model/repositories/UsuarioRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.sena.minhasfinancas.model.repositories;
 
-import javax.persistence.Entity;
+import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,13 +17,13 @@ import com.sena.minhasfinancas.model.entity.Usuario;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@AutoConfigureTestDatabase (replace = Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
 	@Autowired
 	private UsuarioRepository repository;
-	
+
 	@Autowired
 	private TestEntityManager entityManager;
 
@@ -32,7 +32,7 @@ public class UsuarioRepositoryTest {
 
 		// Cenário
 
-		Usuario usuario = Usuario.builder().nome("Marcelo").email("marcelomarcos2@gmail.com").build();
+		Usuario usuario = criarUsuario();
 		entityManager.persist(usuario);
 		// Ação/execução
 		boolean result = repository.existsByEmail("marcelomarcos2@gmail.com");
@@ -41,16 +41,56 @@ public class UsuarioRepositoryTest {
 		Assertions.assertThat(result).isTrue();
 
 	}
-	
+
 	@Test
 	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComEmail() {
-						
+
 		// Ação
 		boolean result = repository.existsByEmail("marcelomarcos2@gmail.com");
-		
-		//Verificação
+
+		// Verificação
 		Assertions.assertThat(result).isFalse();
+
+	}
+
+	@Test
+	public void devePersistirUmUsuarioNaBaseDeDados() {
+		// Cenário
+		Usuario usuario = criarUsuario();
+
+		// Ação
+
+		Usuario usuarioSalvo = repository.save(usuario);
+
+		// Verificação
+		Assertions.assertThat(usuario.getId()).isNotNull();
+	}
+
+	@Test
+	public void deveBuscarUmUsuarioPorEmail() {
+
+		// Cenário
+		Usuario usuario = criarUsuario();
+		entityManager.persist(usuario);
+
+		// Verificação
+		Optional<Usuario> result = repository.findByEmail("marcelomarcos2@gmail.com");
+		Assertions.assertThat(result.isPresent()).isTrue();
+	}
+
+	@Test
+	public void deveRetornarVazioAoBuscarUsuarioPorEmailQuandoNaoExisteNaBase() {
+
+		// Cenário
 		
+		// Verificação
+		Optional<Usuario> result = repository.findByEmail("marcelomarcos2@gmail.com");
+		Assertions.assertThat(result.isPresent()).isFalse();
+	}
+
+	public static Usuario criarUsuario() {
+
+		return Usuario.builder().email("marcelomarcos2@gmail.com").nome("ma").senha("1234").build();
 	}
 
 }
